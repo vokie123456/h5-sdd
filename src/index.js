3cce14e716bb2825575dc7e59498710f346fc157
dany <danywheeler@icloud.com>
2017-04-25 13:44:21 +0800

    render程序入口&HMR

--- src/index.js
+++ src/index.js
@@ -3,13 +3,38 @@
 import 'normalize.css';
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { browserHistory } from 'react-router';
 import { AppContainer } from 'react-hot-loader';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 import configureStore from './store/configureStore';
+import App from './components/App';
 
 const store = configureStore();
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
+const rootEl = document.querySelector('#app');
 
+render(
+  <AppContainer>
+    <Provider store={store}>
+      <App history={history} />
+    </Provider>
+  </AppContainer>,
+  rootEl
+);
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    const NewApp = require('./components/App').default;
+    render(
+      <AppContainer>
+        <Provider store={store}>
+          <NewApp history={history} />
+        </Provider>
+      </AppContainer>,
+      rootEl
+    );
+  });
+}
